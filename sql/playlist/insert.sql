6a4975ae495064dc223ba5880bc4cbe0c837fea2
SpongyBacon <spongybacon@hotmail.co.uk>
2017-01-28 18:32:16 +0000

    update before randon bullies me

--- sql/playlist/insert.sql
+++ sql/playlist/insert.sql
@@ -1,2 +1,1 @@
-INSERT INTO playlists (owner_id, name, date_created) VALUES ($1, $2, $3);
-SELECT currval(pg_get_serial_sequence('playlists', 'id'));
+INSERT INTO playlists (owner_id, name, date_created) VALUES ($1, $2, $3) RETURNING id;
--- sql/song/insert.sql
+++ sql/song/insert.sql
@@ -1,2 +1,1 @@
-INSERT INTO songs (source, identifier, title, author, duration) VALUES ($1, $2, $3, $4, $5);
-SELECT currval(pg_get_serial_sequence('songs', 'id'));
+INSERT INTO songs (source, identifier, title, author, duration) VALUES ($1, $2, $3, $4, $5) RETURNING id;
